70e5b887e92ad00752d12fba2ed7a836b6ec1d11
Robby <robby.olson11@gmail.com>
2019-11-15 08:43:55 -0800

    search done

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,34 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
   const [characters, setCharacters] = useState([]);
 
-  // useEffect(() => {
-  //   axios
-  //     .get(`https://rickandmortyapi.com/api/character/`)
-  //     .then(response => {
-  //       console.log(response);
-  //       setCharacters(response.data.results);
-  //     })
-  //     .catch(error => {
-  //       console.log("something is broken...", error);
-  //     });
-  // }, []);
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        setCharacters(response.data.results);
+      })
+      .catch(error => {
+        console.log("something is broken...", error);
+      });
+  }, []);
 
   return (
     <section className="character-list">
       <h2>Character List</h2>
-      {characters.map(character => {
-        return (
-          <CharacterCard
-            key={character.id}
-            name={character.name}
-            image={character.image}
-          />
-        );
-      })}
+      <SearchForm characters={characters} />
     </section>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,35 @@
 import React, { useState } from "react";
+import CharacterCard from "./CharacterCard";
 
-export default function SearchForm() {
- 
+export default function SearchForm({ characters }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const results = !searchTerm
+    ? characters
+    : characters.filter(character =>
+        character.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+      );
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      {results.map(character => {
+        return (
+          <CharacterCard
+            key={character.id}
+            name={character.name}
+            image={character.image}
+          />
+        );
+      })}
     </section>
   );
 }
